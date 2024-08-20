607c59472adfd757bff603005d4686566a39a844
RishabJha134 <rishabjha134@gmail.com>
2024-08-21 00:11:36 +0530

    add get watch history end point

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -513,7 +513,76 @@ return res
 })
 
 
+const getWatchHistory = asyncHandler(async(req,res)=>{
+  const user = await User.aggregate([
+    {
+      $match:{
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
 
+    },
+
+    {
+      $lookup:{
+        from:"video",
+        localField:"watchHistory",
+        foreignField:"_id",
+        as:"watchHistory",
+
+
+        pipeline:[
+          {
+            $lookup:{
+              from:"users",
+              localField:"owner",
+              foreignField:"_id",
+              as:"owner",
+
+              pipeline:[
+                {
+                  $project:{
+                    fullName:1,
+                    username:1,
+                    avatar:1
+                  }
+                }
+              ]
+            }
+
+          },
+          {
+            $addFields:{
+              owner:{
+                $first:"$owner"
+              }
+            }
+          }
+
+        ]
+
+
+
+      }
+
+    },
+
+   
+
+  ])
+
+
+  return res
+  .status(200)
+  .json(
+      new ApiResponse(
+          200,
+          user[0].watchHistory,
+          "Watch history fetched successfully"
+      )
+  )
+
+  
+})
 
 
 
